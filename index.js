5fb936ec392e2e0ad1d5f15972a3d85d1c5ae8cf
Anton <anton@nlx.ai>
2024-11-20 12:11:35 -0700

    changed lambda

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!')
+    body: JSON.stringify('Hello from Lambda! Updated!')
   };
 };
