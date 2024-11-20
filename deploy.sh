6265cba3f00b1631c5458b5779bf5a860a27810e
Anton <anton@nlx.ai>
2024-11-20 10:20:05 -0700

    updates

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@ zip function.zip index.js
 aws cloudformation update-stack \
   --stack-name rant-lambda-stack \
   --template-body file://templates/main-template.yaml \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameters ParameterKey=S3Bucket,ParameterValue=rant-aws-sample \
-               ParameterKey=S3Key,ParameterValue=function.zip
+  --capabilities CAPABILITY_NAMED_IAM
 
 rm function.zip
