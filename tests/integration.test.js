5d4d8f64d6233cb365b502a6bd4c9d58cffc5f84
Anton <anton@nlx.ai>
2024-11-26 14:02:00 -0700

    fail the test

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -3,6 +3,6 @@ const assert = require('assert');
 describe('Lambda Integration Test', () => {
     it('should return a successful response', async () => {
         const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
-        assert.strictEqual(response.status, 200);
+        assert.strictEqual(response.status, 201);
     });
 });
