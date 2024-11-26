114c369313cc0e5059aed432ddef986f3b750991
Anton <anton@nlx.ai>
2024-11-26 14:40:01 -0700

    update

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -5,4 +5,10 @@ describe('Lambda Integration Test', () => {
         const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
         assert.strictEqual(response.status, 201);
     });
+
+
+    it('second test name', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 203);
+    });
 });
