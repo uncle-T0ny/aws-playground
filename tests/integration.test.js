ca0adbaa87559e5ea109ba8cb5e95d31077da61d
Anton <anton@nlx.ai>
2024-11-26 09:53:26 -0700

    added test step

---
+++ tests/integration.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('Lambda Integration Test', () => {
+    it('should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 200);
+    });
+});
