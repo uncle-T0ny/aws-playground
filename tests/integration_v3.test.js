f63ee2245bc027d9b6ebb1c099865b094d706b63
Anton <anton@nlx.ai>
2024-11-27 15:30:17 -0700

    update

---
+++ tests/integration_v3.test.js
@@ -0,0 +1,101 @@
+const assert = require('assert');
+
+describe('Lambda Integration Test v3', () => {
+    it('the other test v3 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c1 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c2 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c3 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c4 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c5 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c6 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c7 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c8 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c9 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+
+    it('c10 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c11 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c12 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c13 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+
+    it('c14 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c15 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c16 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c17 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+    it('c18 should return a successful response', async () => {
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        assert.strictEqual(response.status, 2001);
+    });
+
+});
