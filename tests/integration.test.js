f36286baa5211411d7c5a9fbb4654be192215766
Anton <anton@nlx.ai>
2024-11-28 13:45:26 -0700

    fixed

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 describe('Lambda Integration Test', () => {
     it('should return a successful response', async () => {
-        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
+        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8?requiredKey=true');
         assert.strictEqual(response.status, 200);
     });
 });
