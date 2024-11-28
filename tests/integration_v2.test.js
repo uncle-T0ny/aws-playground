6567ce6337580578855cbc538da6783be8f5d73a
Anton <anton@nlx.ai>
2024-11-28 10:57:09 -0700

    fixed the tests

--- tests/integration_v2.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-
-describe('Lambda Integration Test v2', () => {
-    it('the other test v2 should return a successful response', async () => {
-        const response = await fetch('https://eb9xvwsprg.execute-api.ca-central-1.amazonaws.com/dev/hello-test-v8');
-        assert.strictEqual(response.status, 2001);
-    });
-});
